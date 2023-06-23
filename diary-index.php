764787c452d5193aeeb73b7c8fdda9d5d1457d72
masaterutagawa <tagawa@wakugumi.co.jp>
2023-06-23 23:27:45 +0900

    課題提出後に色々追加

--- diary-index.php
+++ diary-index.php
@@ -1,67 +1,66 @@
 <?php
-// 入力チェック（受信確認処理追加）
-if (
-    !isset($_POST['today_events']) || $_POST['today_events'] == '' ||
-    !isset($_POST['events_impression']) || $_POST['events_impression'] == '' ||
-    !isset($_POST['events_emotions']) || $_POST['events_emotions'] == '' ||
-    !isset($_POST['events_points']) || $_POST['events_points'] == '' ||
-    !isset($_POST['events_points_reason']) || $_POST['events_points_reason'] == '' ||
-    !isset($_POST['photo_keyword']) || $_POST['photo_keyword'] == '' ||
-    !isset($_POST['photo_keyword_reason']) || $_POST['photo_keyword_reason'] == '' ||
-    !isset($_POST['photo_emotions']) || $_POST['photo_emotions'] == '' ||
-    !isset($_POST['photo_points']) || $_POST['photo_points'] == '' ||
-    !isset($_POST['photo_points_reason']) || $_POST['photo_points_reason'] == ''
-) {
-    exit('ParamError');
-}
-
-// POSTデータ取得
-$today_events = $_POST['today_events'];
-$events_impression = $_POST['events_impression'];
-$events_emotions = $_POST['events_emotions'];
-$events_points = $_POST['events_points'];
-$events_points_reason = $_POST['events_points_reason'];
-$photo_keyword = $_POST['photo_keyword'];
-$photo_keyword_reason = $_POST['photo_keyword_reason'];
-$photo_emotions = $_POST['photo_emotions'];
-$photo_points = $_POST['photo_points'];
-$photo_points_reason = $_POST['photo_points_reason'];
 
 // 関数ファイル読み込み
 include('function.php');
-// LOGIN認証チェック関数を実行
-loginCheck();
+
 // DB接続用関数を実行
 $pdo = db_connect();
 
-
-// データ登録ＳＱＬ作成
-$sql = "INSERT INTO dev13_diary(diary_id,registration_date,created_date,update_date,today_events,events_impression,events_emotions,events_points,events_points_reason,photo_keyword,photo_keyword_reason,photo_emotions,photo_points,photo_points_reason)VALUES(NULL, now(),now(),now(),:today_events, :events_impression, :events_emotions,:events_points, :events_points_reason, :photo_keyword, :photo_keyword_reason, :photo_emotions, :photo_points, :photo_points_reason)";
+// SQL作成&実行
+$sql = "SELECT * FROM dev13_diary WHERE 1";
 $stmt = $pdo->prepare($sql);
 
-// 変数をバインドあら
-$stmt->bindValue(':registration_date', $registration_date, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':created_date', $created_date, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':update_date', $update_date, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':today_events', $today_events, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':events_impression', $events_impression, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':events_emotions', $events_emotions, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':events_points', $events_points, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':events_points_reason', $events_points_reason, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':photo_keyword', $photo_keyword, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':photo_keyword_reason', $photo_keyword_reason, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':photo_emotions', $photo_emotions, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':photo_points', $photo_points, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$stmt->bindValue(':photo_points_reason', $photo_points_reason, PDO::PARAM_STR); // Integer（数値の場合は PDO::PARAM_INT）
-$status = $stmt->execute(); // SQL実行
-
-// データ登録処理後
-if ($status == false) {
-    // SQL実行時にエラーがある場合
-    $error = $stmt->errorInfo();
-    exit("QueryError:" . $error[2]);
-} else {
-    // 登録ページへ移動
-    header('Location: db_list.php');
-    exit;
+try {
+    $status = $stmt->execute();
+} catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
 }
+// SQL実行の処理
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$output = '';
+foreach ($result as $record)
+    $output .= "
+        
+            <a href=\"diary-entry.php?diary_id={$record["diary_id"]}\" class=\"group relative flex h-48 flex-col overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-64 xl:h-96\">
+                <img src=\"card_images/{$record["select_card_filename"]}\" loading=\"lazy\" alt=\"Photo by Minh Pham\" class=\"absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110\" />
+                <div class=\"pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent md:via-transparent\"></div>
+                <div class=\"relative mt-auto p-4\">
+                <span class=\"block text-sm text-gray-200\">{$record["registration_date"]}</span>
+                <h2 class=\"mb-2 text-xl font-semibold text-white transition duration-100\">{$record["today_events"]}</h2>
+                <span class=\"font-semibold text-white\">この日の記録を確認する</span>
+                </div>
+            </a>
+        
+";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>記録</title>
+</head>
+
+<body>
+    <?php include('include/header.php'); ?>
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+            <!-- text - start -->
+            <div class="mb-10 md:mb-16">
+                <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">ガワさんの日記</h2>
+                <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">日記の一覧</p>
+            </div>
+            <!-- text - end -->
+            <div class="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3">
+                <?= $output  ?>
+            </div>
+        </div>
+    </div>
+    <?php include('include/footer.php'); ?>
+</body>
+
+</html>
